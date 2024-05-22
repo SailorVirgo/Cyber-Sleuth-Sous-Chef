21936d883c0c436e6681295c0f3aba313fd89ab1
TravisJBlack <159502505+TravisJBlack@users.noreply.github.com>
2024-05-21 21:01:56 -0500

    added import statments, setup session data, and set handlebars engine

---
+++ server.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+
+const sequelize = require('./config/connections');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({});
+
+const store = new SequelizeStore({
+    db: sequelize
+})
+
+const sess = {
+    secret: 'secret password',
+    cookies: {
+        maxAge: 1000 * 60 * 60 *24,
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: store
+};
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+pp.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
