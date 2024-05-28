6cec6b86badb5f00e2fd0764d74705ab23a55629
SailorVirgo <tm11solarbeam@gmail.com>
2024-05-28 00:28:26 -0500

    deleted all page in views, updated homepage, updated homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -6,8 +6,8 @@ const router = require('express').Router();
 
 // ? GET route for getting all of the dishes that are on the menu
 router.get('/', async (req, res) => {
-  // ? This method is rendering the 'all' Handlebars.js template. This is how we connect each route to the correct template.
-  res.render('all');
+  // ? This method is rendering the 'homepage' Handlebars.js template. This is how we connect each route to the correct template.
+  res.render('homepage');
 });
 
 module.exports = router;
